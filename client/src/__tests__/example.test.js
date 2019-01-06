9d4c1bb9a4241b51c8d0ee7328f8d3e51908c60a
JamieRez <jamierezendes@gmail.com>
2019-01-05 17:50:12 -0800

    Backend init

--- client/src/__tests__/example.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import React, { craete } from "react-test-renderer";
-import Details from "../Details";
-
-test("snapshot", () => {
-  const c = create(<Details />);
-  expect(c.toJson()).toMatchSnapshot();
-});
